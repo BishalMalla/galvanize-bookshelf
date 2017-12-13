b7b910b7605c1276fd6b87c464f2a2089f62e1ed
Bishal Malla <lxixchg@gmail.com>
2017-12-13 07:17:30 -0700

    pushing to heroku attempt 3

--- seeds/books.js
+++ seeds/books.js
@@ -2,7 +2,7 @@
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex('books').del()
-    .then(function () {
+    .then(function() {
         // Inserts seed entries
         return knex('books').insert([{
           id: 1,
